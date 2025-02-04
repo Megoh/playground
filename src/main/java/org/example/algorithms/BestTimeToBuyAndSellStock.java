03d66cacd8e851ecb2f63e522771fa11cff1f6e4
Dominik <dominikwolowicz@gmail.com>
2025-02-04 12:56:17 +0100

    add tests

--- src/main/java/org/example/algorithms/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/example/algorithms/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,18 @@
 package org.example.algorithms;
 
 public class BestTimeToBuyAndSellStock {
+    public static int bestTimeToBuyAndSellStocks(int[] prices) {
 
+        // high - if higher found, set new high. Remember index.
+        // low - if lower found, set new low. Remember index.
+
+        int high, low;
+
+        // iterator
+        for (int i = 0; i < prices.length; i++) {
+
+        }
+
+        return 0;
+    }
 }
---
+++ src/test/java/org/example/algorithms/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,72 @@
+package org.example.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class BestTimeToBuyAndSellStockTest {
+    @Test
+    void testIncreasingPrices() {
+        int[] prices = {1, 3, 4};
+
+        assertEquals(3, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithIncreaseAndDrop() {
+        int[] prices = {1, 3, 4, 0};
+
+        assertEquals(3, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithIncreaseAndDropFollowedByIncrease() {
+        int[] prices = {1, 3, 4, 0, 2};
+
+        assertEquals(3, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithIncreaseDropAndRecovery() {
+        int[] prices = {1, 3, 4, 1, 5};
+
+        assertEquals(4, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithIncreaseDropAndSignificantRecovery() {
+        int[] prices = {1, 3, 4, 2, 6};
+
+        assertEquals(4, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithTwoDrops() {
+        int[] prices = {1, 3, 4, 2, 6, 0};
+
+        assertEquals(4, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithMultipleFluctuations() {
+        int[] prices = {1, 3, 4, 2, 6, 0, 3};
+
+        assertEquals(4, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithMultipleFluctuationsAndIncrease() {
+        int[] prices = {1, 3, 4, 2, 6, 0, 4};
+
+        assertEquals(4, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+
+    @Test
+    void testPricesWithMultipleFluctuationsAndHigherIncrease() {
+        int[] prices = {1, 3, 4, 2, 6, 0, 5};
+
+        assertEquals(5, BestTimeToBuyAndSellStock.bestTimeToBuyAndSellStocks(prices));
+    }
+}
